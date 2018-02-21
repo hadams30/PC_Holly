2d5a1241d8372457fc50c7f1a9e8886314cdc1dd
holly <hadams30@pratt.edu>
2018-02-20 20:58:48 -0500

    second commit SS3

---
+++ SS3_SensitiveDuchamp/ss3.ino
@@ -0,0 +1,32 @@
+/*
+Adafruit Arduino - Lesson 14. Sweep
+*/
+
+#include <Servo.h> 
+
+int servoPin = 9;
+ 
+Servo servo;  
+ 
+int angle = 0;   // servo position in degrees 
+ 
+void setup() 
+{ 
+  servo.attach(servoPin); 
+} 
+ 
+ 
+void loop() 
+{ 
+  // scan from 0 to 180 degrees                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+  for(angle = 0; angle < 30; angle++)  
+  {                                  
+    servo.write(angle);               
+    delay(3);                   
+  } // // now scan back from 180 to 0 degrees
+  for(angle = 30; angle > 0; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(100);       
+  } 
+} 
